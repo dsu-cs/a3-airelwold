cc45acf5a14b3842ed721c2d48b114f2294b5681
airelwold <momma.wolf.wold@gmail.com>
2019-11-16 22:48:31 -0600

    Implemented and tested the search function 2/3 pass

--- src/bst.hpp
+++ src/bst.hpp
@@ -5,8 +5,9 @@
 #include <iostream>
 #include "node.hpp"
 
-template<class T>
-class BST{
+template <class T>
+class BST
+{
 public:
     // Constructor for the BST class, creates an empty tree
     BST(void);
@@ -35,80 +36,136 @@ public:
     // Gets the current number of nodes in the tree
     // returns: the number of nodes in the tree
     int get_size(void);
+
 private:
     // the root node of the tree
     Node<T> *root;
     // the number of nodes in the tree
-    int node_count;
+    int i;
 };
 
-template<class T>
+template <class T>
 BST<T>::BST()
 {
     root = NULL;
-    node_count = 0;
+    i = 0;
 }
 
-template<class T>
+template <class T>
 BST<T>::~BST()
 {
     root = NULL;
-    while(root != NULL)
+    while (root != NULL)
     {
         remove(root->get_data());
     }
 }
 
-template<class T>
- std::vector<T> * BST<T>::inorder()
+template <class T>
+std::vector<T> *BST<T>::inorder()
+{
+    std::vector<T> *vec = new std::vector<T>;
+
+    /*if (root != NULL)
+    {
+        root->get_left;
+        root->get_right;
+    }*/
+    
+
+    /*if (begin > end)
+    {
+        return NULL;
+    }
+
+    i = max (inorder, begin, end);
+    Node *root = new(inorder[i]);
+
+    if (begin == end)
+    {
+        return root;
+    }
+
+    root->getleft() = BST (inorder, begin, i - 1);  
+    root->getright() = BST (inorder, i + 1, end); */ 
+
+    return vec;
+}
+
+template <class T>
+std::vector<T> *BST<T>::preorder()
+{
+    std::vector<T> *vec = new std::vector<T>;
+    return vec;
+}
+
+template <class T>
+std::vector<T> *BST<T>::postorder()
 {
     std::vector<T> *vec = new std::vector<T>;
 
     return vec;
 }
 
-
-template<class T>
- std::vector<T> * BST<T>::preorder()
-{
-    std::vector<T> *vec = new std::vector<T>;
-    return vec;
-}
-
-
-template<class T>
- std::vector<T> * BST<T>::postorder()
-{
-    std::vector<T> *vec = new std::vector<T>;
-
-    return vec;
-}
-
-template<class T>
+template <class T>
 void BST<T>::insert(T new_data)
 {
-
+    /*if (root == NULL)
+    {
+        root = createNode(new_data);
+    }
+    else if (new_data<=root->data)
+    {
+        root->left=insert(root->left, new_data);
+    }
+    else
+    {
+        root->right=insert(root->right, new_data);
+    }
+    return root;*/
+    //Node<T>* BST<T>::search(T) [width T = int];
 }
 
-
-template<class T>
+template <class T>
 Node<T> *BST<T>::search(T val)
 {
+    while (root != NULL)
+    {
+        if (root->get_data() == val)
+        {
+            break;
+        }
+        root = root->get_left();
+        root = root->get_right();
+    }
+    return root;
+    
+    /*while (root != NULL)
+    {
+        if (root->get_data() == val)
+        {
+        return(root);
+        }
+        else if (i )
+        {
+            root->get_left();
+        }
+        else
+        {
+            root->get_right();
+        }
+    return(NULL);
+    }*/
 
 }
 
-
-
-template<class T>
+template <class T>
 void BST<T>::remove(T val)
 {
-
 }
 
-
-
-template<class T>
+template <class T>
 int BST<T>::get_size()
 {
-
+    
 }
