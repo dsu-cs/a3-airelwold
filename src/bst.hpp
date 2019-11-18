2272925cbd4c517abe3c090d280c86995a6129ab
airelwold <momma.wolf.wold@gmail.com>
2019-11-18 00:47:33 -0600

    Implemented search function

--- src/bst.hpp
+++ src/bst.hpp
@@ -41,14 +41,14 @@ private:
     // the root node of the tree
     Node<T> *root;
     // the number of nodes in the tree
-    int i;
+    int node_count;
 };
 
 template <class T>
 BST<T>::BST()
 {
     root = NULL;
-    i = 0;
+    node_count = 0;
 }
 
 template <class T>
@@ -66,11 +66,20 @@ std::vector<T> *BST<T>::inorder()
 {
     std::vector<T> *vec = new std::vector<T>;
 
-    /*if (root != NULL)
+    /*if (root == NULL)
     {
-        root->get_left;
-        root->get_right;
-    }*/
+        return;
+    }
+     
+    inorder(root.get_left());
+    inorder(root.get_right());*/
+    
+
+    if (root != NULL)
+    {
+        root->get_left();
+        root->get_right();
+    }
     
 
     /*if (begin > end)
@@ -86,8 +95,8 @@ std::vector<T> *BST<T>::inorder()
         return root;
     }
 
-    root->getleft() = BST (inorder, begin, i - 1);  
-    root->getright() = BST (inorder, i + 1, end); */ 
+    root->get_left() = BST (inorder, begin, i - 1);  
+    root->get_right() = BST (inorder, i + 1, end); */ 
 
     return vec;
 }
@@ -110,43 +119,82 @@ std::vector<T> *BST<T>::postorder()
 template <class T>
 void BST<T>::insert(T new_data)
 {
-    /*if (root == NULL)
+    /*Node<T> *createNode;
+    int *i;
+
+    i = 0;
+
+    if (new_data == NULL)
     {
-        root = createNode(new_data);
+        return;
     }
-    else if (new_data<=root->data)
+    else if (new_data < root->i)
     {
-        root->left=insert(root->left, new_data);
+        root->get_left(), i;
     }
     else
     {
-        root->right=insert(root->right, new_data);
+        root->get_right(), i;
     }
-    return root;*/
+    return;*/
     //Node<T>* BST<T>::search(T) [width T = int];
 }
 
 template <class T>
 Node<T> *BST<T>::search(T val)
 {
+    /*int left = 0;
+    int right = 0;
+   if (root == NULL)
+   {
+       return NULL;
+   }
+   else if (root->get_data() == val)
+    {
+        return root;
+    }
+    else
+    {
+        Node<T> *left = search(root->get_left(), val);
+        Node<T> *right = search(root->get_right(), val);
+
+        if (left == 0 && right == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return left+right+1;
+        }
+    }*/
+
+    /*int left = 0;
+    int right = 0;
+   if (root == NULL)
+   {
+       return 1;
+   }
+   else if (root->get_data() == val)
+    {
+        return 1;
+    }
+    else if (root->get_data() > val)
+    {
+        return search(root->get_left(), val);
+    }
+    else
+    {
+        return search(root->get_right(), val);
+    }*/
+    Node<T> *cur_Node;
+
     while (root != NULL)
     {
         if (root->get_data() == val)
         {
             break;
         }
-        root = root->get_left();
-        root = root->get_right();
-    }
-    return root;
-    
-    /*while (root != NULL)
-    {
-        if (root->get_data() == val)
-        {
-        return(root);
-        }
-        else if (i )
+        else if (root->get_data() != val)
         {
             root->get_left();
         }
@@ -154,18 +202,95 @@ Node<T> *BST<T>::search(T val)
         {
             root->get_right();
         }
-    return(NULL);
-    }*/
+    return root;
+    }
 
 }
 
 template <class T>
 void BST<T>::remove(T val)
 {
+    Node<T> *temp_Node = NULL;
+    Node<T> *cur_Node = root;
+
+    while(cur_Node != NULL)
+    {
+        if(cur_Node->get_data() == val)
+        {
+            if(!cur_Node->get_left() && !cur_Node->get_right())
+            {
+                 if (!temp_Node)
+                 {
+                     root = NULL;
+                 }
+                 else if (temp_Node->get_left() == cur_Node)
+                 {
+                     temp_Node->set_left(NULL);
+                 }
+                 else
+                 {
+                     temp_Node->set_right(NULL);
+                 } 
+            }
+            else if (cur_Node->get_left() && !cur_Node->get_right())
+            {
+                 if (!temp_Node)
+                 {
+                     root = cur_Node->get_left();
+                 }
+                 else if (temp_Node->get_left() == cur_Node)
+                 {
+                     temp_Node->set_left(cur_Node->get_left());
+                 }
+                 else
+                 {
+                     temp_Node->set_right(cur_Node->get_left());
+                 } 
+            }
+            else if (!cur_Node->get_left() && cur_Node->get_right())
+            {
+                if (!temp_Node)
+                {
+                    root = cur_Node->get_right();
+                }   
+                else if (temp_Node->get_left() == cur_Node)
+                {
+                      temp_Node->set_left(cur_Node->get_right());
+                }
+                 else
+                {
+                    temp_Node->set_right(cur_Node->get_right());
+                } 
+            }
+            else
+            {
+                Node<T> *suc_Node = cur_Node->get_right();
+                while (suc_Node->get_left() != NULL)
+                {
+                    suc_Node = suc_Node->get_left();
+                    int suc_Data = suc_Node->get_data();
+                    remove(suc_Node->get_data());
+                    suc_Node->set_data(suc_Data);
+                }
+                return;
+            }
+        }
+        else if (cur_Node->get_data() < val)
+        {
+            temp_Node = cur_Node;
+            cur_Node = cur_Node->get_right();
+        }
+        else
+        {
+            temp_Node = cur_Node;
+            cur_Node = cur_Node->get_left();
+        }
+    }         
+    return;
 }
 
 template <class T>
 int BST<T>::get_size()
 {
-    
+       
 }
